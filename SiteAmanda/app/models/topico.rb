2e84796e778566ccf4920d51ac1a3ed4fbf54456
Márcio Ferreira <cocolaman@hotmail.com>
2018-12-04 03:23:04 -0200

    Adicionada funcionalidade de Topicos

--- SiteAmanda/app/models/topico.rb
+++ SiteAmanda/app/models/topico.rb
@@ -1,2 +1,6 @@
 class Topico < ApplicationRecord
+	validates :titulo, presence: true
+	validates_length_of :titulo, minimum: 5, maximum: 50
+	validates :corpo, presence: true
+	validates_length_of :corpo, minimum: 10, maximum: 180
 end
--- SiteAmanda/db/schema.rb
+++ SiteAmanda/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181202003546) do
+ActiveRecord::Schema.define(version: 20181202022918) do
 
   create_table "formularios", force: :cascade do |t|
     t.string "email"
