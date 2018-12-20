6bc98c532098774c9bf05d75d548d30dd29d254b
DanSales <plantice2@gmail.com>
2018-12-20 10:16:01 -0300

    Adicionando os validates de Comentario

--- SiteAmanda/app/models/comentario.rb
+++ SiteAmanda/app/models/comentario.rb
@@ -1,4 +1,6 @@
 class Comentario < ApplicationRecord
   belongs_to :topico
   validates :usuario, presence: true
+  validates :texto, presence: true
+  validates_length_of :texto, minimum: 3, maximum: 1000
 end
