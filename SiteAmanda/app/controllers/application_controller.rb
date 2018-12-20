35f07fac8cbf7242e3352d153f77e795f931deba
DanSales <plantice2@gmail.com>
2018-12-20 10:22:38 -0300

    Refatorando application_controller com metodo de manutencao dos controles

--- SiteAmanda/app/controllers/application_controller.rb
+++ SiteAmanda/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+def manipulacaoControle(tipo, condicao, classe, funcao, status, casoContrario)
+  respond_to do |format|
+    if condicao
+      format.html {redirect_to tipo, notice: classe + ' was sucessfully ' + funcao}
+      format.json {render :show, status: status, location: tipo}
+    else
+      format.html {render casoContrario}
+      format.json {render json: tipo.errors, status: :unprocessable_entity}
+    end
+
+  end
+end
